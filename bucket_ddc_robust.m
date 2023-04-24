48670bc51469bea85f6b98fabcd2f113849f58f4
Jared Miller <jared.fr.miller@gmail.com>
2023-04-24 12:22:10 -0400

    initial commit of bucket code

---
+++ bucket_ddc_robust.m
@@ -0,0 +1,95 @@
+rng(20, 'twister')
+n = 3;
+m = 2;
+% ss = rss(n, n, m);
+A0 = 0.6*eye(3)+ 0.1*ones(3);
+% A0(3, 2) = 1.3;
+A0(3, 2) = 1.1;
+B0 = [1 0; 1 0; 0 1];
+ss = struct('A', A0, 'B', B0);
+
+%% sample data
+T = 300;
+umax = 1;
+Xn = 2*rand(n, T)-1;
+U = umax*(2*rand(m, T)-1);
+Xp_true = A0*Xn + B0*U;
+epsilon = 0.05;
+eps_noise = epsilon*(2*rand(n, T)-1);
+Xp = eps_noise + Xp_true;
+
+
+% buckets = [-inf, -1; -1, 0; 0, -1; 1, inf];
+
+% 
+% buckets = [-inf, -1; -1, 1; 1, inf];
+buckets = [-inf, -1; -1, 0; 0, 1; 1, inf];
+Nbucket = size(buckets, 1);
+Sb = cell(Nbucket, 1);
+for i = 1:Nbucket
+    Sb{i} = (Xp >= buckets(i, 1))  & (Xp <= buckets(i, 2));
+end
+
+
+
+%% synthesize a controller
+% ss.A = 1.5*ss.A;
+
+K = sdpvar(m, n, 'full');
+A = sdpvar(n, n, 'full');
+B = sdpvar(n, m, 'full');
+dx = sdpvar(n, T, 'full');
+
+Xp_curr = A*Xn + B*U + dx;
+Xp_sub = replace(Xp_curr, [A(:); B(:); dx(:)], [A0(:); B0(:); eps_noise(:)]);
+
+rob_vars = [A(:); B(:)];
+rob_vars_all = [A(:); B(:); dx(:)];
+M0 = sdpvar(n, n);
+Ma = cell(length(rob_vars));
+
+%affine adjustable robust counterpart
+M = M0;
+for i = 1:length(rob_vars)
+    Ma{i} = sdpvar(n, n);
+    M = M +  Ma{i}*rob_vars(i);
+end
+
+
+dd_term = [];
+for i = 1:Nbucket
+    if buckets(i, 1) > -inf
+        dd_low = Xp_curr - buckets(i, 1);
+        dd_term = [dd_term; dd_low(Sb{i})];
+    end
+    if buckets(i, 2) < inf
+        dd_high = (buckets(i, 2) - Xp_curr);
+        dd_term = [dd_term; dd_high(Sb{i})];
+    end
+end
+dd_con = (dd_term>=0);
+
+rob_con = [epsilon >= dx(:); epsilon >= -dx(:)];
+
+%(super) stabilization
+Acl = A+B*K;
+delta = 1e-2;
+
+ss_con = [(M(:)-Acl(:)) >=0 ; (M(:)+Acl(:))>= 0; sum(M, 2) <= (1-delta)];
+
+
+% M = sdpvar(n, n);
+
+cons = [ss_con:'superstability'; dd_con:'buckets'; rob_con:'robust epsilon'; uncertain(rob_vars_all)];
+opts = sdpsettings('robust.lplp', 'duality');
+sol = optimize(cons, [], opts);
+
+disp(sol.info)
+
+%% recovery
+% Mr = value(M);
+M_rep = replace(M, rob_vars, [A0(:); B0(:)]);
+Mr = value(M_rep);
+Kr = value(K);
+Aclr = value(Acl);
+Aclr0 = value(A0 + B0*Kr);
---
+++ super_clean_test.m
@@ -0,0 +1,23 @@
+rng(20, 'twister')
+n = 3;
+m = 2;
+% ss = rss(n, n, m);
+A0 = 0.6*eye(3)+ 0.1*ones(3);
+A0(3, 2) = 2;
+B0 = [1 0; 0 0; 0 1];
+ss = struct('A', A0, 'B', B0)
+% ss.A = 1.5*ss.A;
+
+K = sdpvar(m, n);
+
+Acl = ss.A+ss.B*K;
+M = sdpvar(n, n);
+delta = 1e-2;
+cons = [M(:)- Acl(:) >=0 ; M(:) +Acl(:)>= 0; sum(M, 2) <= (1-delta)];
+opts = sdpsettings;
+sol = optimize(cons, [], opts);
+
+%% recovery
+Mr = value(M);
+Kr = value(K);
+Aclr = value(Acl);
